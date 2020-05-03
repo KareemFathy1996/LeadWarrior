34545f815c3621645df78128184c0a2d5dcf6a51
KareemFathy1996 <karemfathy1996@yahoo.com>
2020-05-04 00:38:35 +0200

    add hero class js and choose hero in room

--- js/global.js
+++
@@ -1,3 +0,0 @@
-function openLogin() {
-    gameDiv = document.getElementById("game")
-}
---
+++ js/hero.js
@@ -0,0 +1,40 @@
+class Hero {
+    constructor(src, name, health, attackSpeed, movementSpeed, numOfBullets) {
+        this.src = src;
+        this.name = name;
+        this.health = health;
+        this.attackSpeed = attackSpeed;
+        this.movementSpeed = movementSpeed;
+        this.numOfBullets = numOfBullets;
+    }
+}
+
+var heros = [
+    new Hero("assets/pikachu.png", "Pikachu", 50, 0.75, 10, 5),
+    new Hero("assets/snorlax.png", "Snorlax", 1000, 0.2, 5, 2),
+    new Hero("assets/bullbasaur.png", "Bullbasaur", 500, 0.75, 7, 3),
+]
+
+function next() {
+    i++
+    if (i >= heros.length) i = 0;
+    updateHero(heros[i])
+}
+
+function pre() {
+    i--
+    if (i < 0) i = heros.length - 1
+    updateHero(heros[i])
+}
+
+function updateHero(hero) {
+    document.getElementById("choose-hero-img").src = hero.src;
+    document.getElementById("choose-hero-name").innerHTML = hero.name;
+    document.getElementById("health").innerHTML = hero.health;
+    document.getElementById("attackSpeed").innerHTML = hero.attackSpeed;
+    document.getElementById("movementSpeed").innerHTML = hero.movementSpeed;
+    document.getElementById("numberOfBullets").innerHTML = hero.numOfBullets;
+}
+
+var i = 0;
+updateHero(heros[i])
