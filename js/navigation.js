8fff8e5bf33edfb82425a9de07d45a08924c70bf
KareemFathy1996 <karemfathy1996@yahoo.com>
2020-05-14 14:52:33 +0200

    create empty how-to-play & credits pages

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,6 @@
 var currentPage = "main-menu";
 
+/* main menu page */
 function showMainMenuPage() {
     document.getElementById("main-menu").style.display = "block";
     document.getElementById("game-menu-title").style.display = "block";
@@ -11,6 +12,59 @@ function hideMainMenu() {
     document.getElementById("game-menu-title").style.display = "none";
 }
 
+/* how to play page */
+function initHowToPlay() {
+    var images = [];
+    images.push(document.getElementById("how-to-play-img").src);
+
+    var preloadDone = function() {
+        showHowToPlay();
+        removeLoadingScreen();
+    };
+
+    preloadImages(images, preloadDone);
+}
+
+function showHowToPlay() {
+    document.getElementById("how-to-play").style.display = "block";
+    document.getElementById("prev").style.display = "block";
+    document.getElementById("prev").onclick = function() {
+        changePage("main-menu");
+    }
+}
+
+function hideHowToPlay() {
+    document.getElementById("how-to-play").style.display = "none";
+    document.getElementById("prev").style.display = "none";
+}
+
+/* credits page */
+function initCredits() {
+    var images = [];
+    images.push(document.getElementById("credits-img").src);
+
+    var preloadDone = function() {
+        showCredits();
+        removeLoadingScreen();
+    };
+
+    preloadImages(images, preloadDone);
+}
+
+function showCredits() {
+    document.getElementById("credits").style.display = "block";
+    document.getElementById("prev").style.display = "block";
+    document.getElementById("prev").onclick = function() {
+        changePage("main-menu");
+    }
+}
+
+function hideCredits() {
+    document.getElementById("credits").style.display = "none";
+    document.getElementById("prev").style.display = "none";
+}
+
+/* generic function */
 function changePage(next) {
     // show loading
     showLoadingScreen();
@@ -20,6 +74,10 @@ function changePage(next) {
         hideRoom();
     else if (currentPage == "game")
         hideGame();
+    else if (currentPage == "how-to-play")
+        hideHowToPlay();
+    else if (currentPage == "credits")
+        hideCredits();
     else
         hideMainMenu();
 
@@ -30,6 +88,10 @@ function changePage(next) {
         nextFunction = initRoomPage;
     else if (next == 'game')
         nextFunction = initGamePage;
+    else if (next == "how-to-play")
+        nextFunction = initHowToPlay;
+    else if (next == "credits")
+        nextFunction = initCredits;
     else
         nextFunction = showMainMenuPage;
 
