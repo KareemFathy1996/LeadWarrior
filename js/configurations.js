069409be31f5f4a8d902247ee4626ba1875f1cc0
KareemFathy1996 <karemfathy1996@yahoo.com>
2020-05-14 03:41:49 +0200

    change lines in configuration

--- js/configurations.js
+++ js/configurations.js
@@ -1,11 +1,19 @@
 // the only place to edit to affect gameplay
 
-var heroWidth = 50;
-var heroHeight = 50;
-
 var loopInterval = 15;
 
 var loadingScreenTime = 300;
+
+var gameInitialWidth = 1800;
+var gameInitialHeight = 900;
+
+var minNewAngleTime = 500;
+var maxNewAngleTime = 1500;
+
+/* will be removed */
+
+var heroWidth = 50;
+var heroHeight = 50;
 
 var enemyWidth = 50;
 var enemyHeight = 50;
@@ -17,9 +25,3 @@ var bulletWidth = 25;
 var bulletHeight = 25;
 var bulletMovementSpeed = 4;
 var bulletDamage = 10;
-
-var minNewAngleTime = 500;
-var maxNewAngleTime = 1500;
-
-var gameInitialWidth = 1000;
-var gameInitialHeight = 500;
